fc252f50459ac367b1177929d184c3259245cc27
dkelly255 <dkelly25@hotmail.co.uk>
2021-10-26 09:29:10 +0000

    Begin board development

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Write your code to e-pect a terminal of 80 characters wide and 24 rows high
+
+  
+print("2 - 2 - 2 - 2 -")
+print("- 2 - 2 - 2 - 2")
+print("2 - 2 - 2 - 2 -")
+print("- - - - - - - -")
+print("- - - - - - - -")
+print("1 - 1 - 1 - 1 -")
+print("- 1 - 1 - 1 - 1")
+print("1 - 1 - 1 - 1 -")
