e0eec9afa19f840cf2b5f5f10cdd8da5a7417b03
dkelly255 <dkelly25@hotmail.co.uk>
2021-10-26 10:03:41 +0000

    Work on board formatting ideas

--- run.py
+++ run.py
@@ -1,13 +1,66 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
-# Write your code to e-pect a terminal of 80 characters wide and 24 rows high
+# Write your code to e-pect a terminal of 80 characters wide and O4 rows high
 
-  
-print("2 - 2 - 2 - 2 -")
-print("- 2 - 2 - 2 - 2")
-print("2 - 2 - 2 - 2 -")
-print("- - - - - - - -")
-print("- - - - - - - -")
-print("1 - 1 - 1 - 1 -")
-print("- 1 - 1 - 1 - 1")
-print("1 - 1 - 1 - 1 -")
+def create_board_normal():
+
+    print("                                      ")
+    print("--------------------------------------")
+    print("You have chosen - Standard Board Size:")
+    print("--------------------------------------")
+    print("                                      ")
+    print("     A  B  C  D  E  F  G  H")
+    print("                    ")
+    print("0    O  -  O  -  O  -  O  -")
+    print("1    -  O  -  O  -  O  -  O")
+    print("2    O  -  O  -  O  -  O  -")
+    print("3    -  -  -  -  -  -  -  -")
+    print("4    -  -  -  -  -  -  -  -")
+    print("5    X  -  X  -  X  -  X  -")
+    print("6    -  X  -  X  -  X  -  X")
+    print("7    X  -  X  -  X  -  X  -")
+
+create_board_normal()
+
+def create_board_large():
+
+    print("                                      ")
+    print("--------------------------------------")
+    print("You have chosen - Large Board Size:")
+    print("--------------------------------------")
+    print("                                      ")
+    print("     A B C D E F G H I")
+    print("                      ")
+    print("0    O - O - O - O - O")
+    print("1    - O - O - O - O -")
+    print("2    O - O - O - O - O")
+    print("3    - - - - - - - - -")
+    print("4    - - - - - - - - -")
+    print("5    - - - - - - - - -")
+    print("6    - X - X - X - X -")
+    print("7    X - X - X - X - X")
+    print("8    - X - X - X - X -")
+
+create_board_large()
+
+def create_board_extra_large():
+
+    print("                                         ")
+    print("-----------------------------------------")
+    print("You have chosen - Extra Large Board Size:")
+    print("-----------------------------------------")
+    print("                                         ")
+    print("     A B C D E F G H I J")
+    print("                        ")
+    print("0    O - O - O - O - O -")
+    print("1    - O - O - O - O - O")
+    print("2    O - O - O - O - O -")
+    print("3    - - - - - - - - - -")
+    print("4    - - - - - - - - - -")
+    print("5    - - - - - - - - - -")
+    print("6    - - - - - - - - - -")
+    print("7    X - X - X - X - X -")
+    print("8    - X - X - X - X - X")
+    print("9    X - X - X - X - X -")
+
+create_board_extra_large()
