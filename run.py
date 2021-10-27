5eb61ab49e7bd2c5ef27b2ffd658e26d0ac16011
dkelly255 <dkelly25@hotmail.co.uk>
2021-10-27 07:32:32 +0000

    Develop early menu screens

--- run.py
+++ run.py
@@ -8,6 +8,122 @@ def clear():
     else:
         _ = system('clear')
 
+# game_mode_status = "NOT DONE"
+# game_mode = "X"
+
+def select_board_normal():
+
+    print("                                      ")
+    print("--------------------------------------------")
+    print("You have chosen Board Size: Standard (8 x 8)")
+    print("--------------------------------------------")
+    print("|                                          |")
+    print("|           A  B  C  D  E  F  G  H         |")
+    print("|                                          |")
+    print("|      0    O  -  &  -  O  -  0  -         |")
+    print("|      1    -  O  -  O  -  @  -  O         |")
+    print("|      2    O  -  O  -  O  -  O  -         |")
+    print("|      3    -  -  -  -  -  -  -  -         |")
+    print("|      4    -  -  -  -  -  -  -  -         |")
+    print("|      5    X  -  X  -  X  -  X  -         |")
+    print("|      6    -  X  -  X  -  X  -  X         |")
+    print("|      7    #  -  X  -  X  -  X  -         |")
+    print("|                                          |")
+    print("|                                          |")
+    print("|        [PRESS ENTER TO CONTINUE]         |")
+    print("|__________________________________________|")
+    input("")
+    clear()
+    setup_screen()
+
+def select_board_large():
+
+    print("                                      ")
+    print("-------------------------------------------")
+    print("You have chosen Board Size: Large (9 x 9)  ")
+    print("-------------------------------------------")
+    print("|                                          |")
+    print("|           A  B  C  D  E  F  G  H  I      |")
+    print("|                                          |")
+    print("|      0    O  -  O  -  O  -  O  -  O      |")
+    print("|      1    -  O  -  O  -  O  -  O  -      |")
+    print("|      2    O  -  O  -  O  -  O  -  O      |")
+    print("|      3    -  -  -  -  -  -  -  -  -      |")
+    print("|      4    -  -  -  -  -  -  -  -  -      |")
+    print("|      5    -  -  -  -  -  -  -  -  -      |")
+    print("|      6    -  X  -  X  -  X  -  X  -      |")
+    print("|      7    X  -  X  -  X  -  X  -  X      |")
+    print("|      8    -  X  -  X  -  X  -  X  -      |")
+    print("|                                          |")
+    print("|                                          |")
+    print("|        [PRESS ENTER TO CONTINUE]         |")
+    print("|__________________________________________|")
+    input("")
+    clear()
+    setup_screen()
+
+
+def select_board_extralarge():
+
+    print("                                         ")
+    print("-------------------------------------------------")
+    print("You have chosen Board Size: Extra-Large (10 x 10)")
+    print("-------------------------------------------------")
+    print("|                                               |")
+    print("|                                               |")
+    print("|           A  B  C  D  E  F  G  H  I  J        |")
+    print("|                                               |")
+    print("|      0    O  -  O  -  O  -  O  -  O  -        |")
+    print("|      1    -  O  -  O  -  O  -  O  -  O        |")
+    print("|      2    O  -  O  -  O  -  O  -  O  -        |")
+    print("|      3    -  -  -  -  -  -  -  -  -  -        |")
+    print("|      4    -  -  -  -  -  -  -  -  -  -        |")
+    print("|      5    -  -  -  -  -  -  -  -  -  -        |")
+    print("|      6    -  -  -  -  -  -  -  -  -  -        |")
+    print("|      7    X  -  X  -  X  -  X  -  X  -        |")
+    print("|      8    -  X  -  X  -  X  -  X  -  X        |")
+    print("|      9    X  -  X  -  X  -  X  -  X  -        |")
+    print("|                                               |")
+    print("|                                               |")
+    print("|        [PRESS ENTER TO CONTINUE]              |")
+    print("|_______________________________________________|")
+    input("")
+    clear()
+    setup_screen()
+
+
+def select_board_size():
+
+    print("--------------------------------------------")
+    print("|                                          |")
+    print("|************SELECT BOARD SIZE*************|")
+    print("|                                          |")
+    print("|                                          |")
+    print("|    A. STANDARD                           |")
+    print("|                                          |")
+    print("|    B. LARGE                              |")
+    print("|                                          |")
+    print("|    C. EXTRA-LARGE                        |")
+    print("|                                          |")
+    print("|                                          |")
+    print("|                                          |")
+    print("|            [PLEASE SELECT]               |")
+    print("|                                          |")
+    print("--------------------------------------------")
+    setup = input("Select option A, B or C: ")
+    if setup.upper() == "A":
+        clear()
+        select_board_normal()
+    elif setup.upper() == "B":            
+        clear()        
+        select_board_large()
+    elif setup.upper() == "C":            
+        clear()        
+        select_board_extralarge()
+        
+    else:
+        print("Please make a valid choice")
+
 def select_game_mode():
 
     print("--------------------------------------------")
@@ -27,6 +143,58 @@ def select_game_mode():
     print("|                                          |")
     print("--------------------------------------------")
     setup = input("Select option A or B: ")
+    if setup.upper() == "A":
+        clear()
+        game_mode_A()    
+    elif setup.upper() == "B":            
+        clear()        
+        game_mode_B()
+    else:
+        print("Please make a valid choice")
+
+
+def game_mode_A():
+    print("--------------------------------------------")
+    print("|                                          |")
+    print("|************SELECT GAME MODE**************|")
+    print("|                                          |")
+    print("|                                          |")
+    print("|      YOU HAVE SELECTED GAME MODE: A      |")
+    print("|                                          |")
+    print("|                                          |")
+    print("|        PLAYER1    vs.   COMPUTER         |")
+    print("|                                          |")
+    print("|                                          |")
+    print("|                                          |")
+    print("|        [PRESS ENTER TO CONTINUE]         |")
+    print("|                                          |")
+    print("|                                          |")
+    print("--------------------------------------------")
+    input("")
+    clear()
+    setup_screen()
+
+def game_mode_B():
+    print("--------------------------------------------")
+    print("|                                          |")
+    print("|************SELECT GAME MODE**************|")
+    print("|                                          |")
+    print("|                                          |")
+    print("|      YOU HAVE SELECTED GAME MODE: B      |")
+    print("|                                          |")
+    print("|                                          |")
+    print("|        PLAYER1    vs.   PLAYER2          |")
+    print("|                                          |")
+    print("|                                          |")
+    print("|                                          |")
+    print("|        [PRESS ENTER TO CONTINUE]         |")
+    print("|                                          |")
+    print("|                                          |")
+    print("--------------------------------------------")
+    input("")
+    clear()
+    setup_screen()
+
 
 def welcome_screen():
     print("--------------------------------------------")
@@ -44,8 +212,7 @@ def welcome_screen():
     print("|         [PRESS ENTER TO BEGIN]           |")
     print("|                                          |")
     print("|                                          |")
-    print("--------------------------------------------")
-    
+    print("--------------------------------------------")    
 
 
 welcome_screen()
@@ -76,86 +243,15 @@ def setup_screen():
 
     if setup.upper() == "A":
         clear()
-        sleep(0.5)
         select_game_mode()
+    elif setup.upper() == "B":
+        clear()
+        select_board_size()
     else:
         print("Please make a valid choice")
 
 setup_screen()
 
 
-def create_board_normal():
-
-    print("                                      ")
-    print("--------------------------------------------")
-    print("You have chosen Board Size: Standard (8 x 8)")
-    print("--------------------------------------------")
-    print("                                      ")
-    print("                                      ")
-    print("           A  B  C  D  E  F  G  H")
-    print("                                 ")
-    print("      0    O  -  &  -  O  -  0  -")
-    print("      1    -  O  -  O  -  @  -  O")
-    print("      2    O  -  O  -  O  -  O  -")
-    print("      3    -  -  -  -  -  -  -  -")
-    print("      4    -  -  -  -  -  -  -  -")
-    print("      5    X  -  X  -  X  -  X  -")
-    print("      6    -  X  -  X  -  X  -  X")
-    print("      7    #  -  X  -  X  -  X  -")
-    print("                                      ")
-
-
-# create_board_normal()
-
-
-def create_board_large():
-
-    print("                                      ")
-    print("-----------------------------------------")
-    print("You have chosen Board Size: Large (9 x 9)")
-    print("-----------------------------------------")
-    print("                                      ")
-    print("                                      ")
-    print("           A  B  C  D  E  F  G  H  I")
-    print("                            ")
-    print("      0    O  -  O  -  O  -  O  -  O")
-    print("      1    -  O  -  O  -  O  -  O  -")
-    print("      2    O  -  O  -  O  -  O  -  O")
-    print("      3    -  -  -  -  -  -  -  -  -")
-    print("      4    -  -  -  -  -  -  -  -  -")
-    print("      5    -  -  -  -  -  -  -  -  -")
-    print("      6    -  X  -  X  -  X  -  X  -")
-    print("      7    X  -  X  -  X  -  X  -  X")
-    print("      8    -  X  -  X  -  X  -  X  -")
-    print("                                      ")
-
-
-# create_board_large()
-
-
-def create_board_extra_large():
-
-    print("                                         ")
-    print("-------------------------------------------------")
-    print("You have chosen Board Size: Extra-Large (10 x 10)")
-    print("-------------------------------------------------")
-    print("                                         ")
-    print("                                      ")
-    print("           A  B  C  D  E  F  G  H  I  J")
-    print("                                 ")
-    print("      0    O  -  O  -  O  -  O  -  O  -")
-    print("      1    -  O  -  O  -  O  -  O  -  O")
-    print("      2    O  -  O  -  O  -  O  -  O  -")
-    print("      3    -  -  -  -  -  -  -  -  -  -")
-    print("      4    -  -  -  -  -  -  -  -  -  -")
-    print("      5    -  -  -  -  -  -  -  -  -  -")
-    print("      6    -  -  -  -  -  -  -  -  -  -")
-    print("      7    X  -  X  -  X  -  X  -  X  -")
-    print("      8    -  X  -  X  -  X  -  X  -  X")
-    print("      9    X  -  X  -  X  -  X  -  X  -")
-    print("                                      ")
-
-
-# create_board_extra_large()
 
 
