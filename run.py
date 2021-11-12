30f4d594a240a6e2896f34a028d24b5044648558
dkelly255 <dkelly25@hotmail.co.uk>
2021-11-12 15:59:24 +0000

    Amend line spacing for CI terminal display

--- run.py
+++ run.py
@@ -112,8 +112,8 @@ def question_1(revenue, revenue_delta, expenses, expense_delta, profits, profit_
             break
     scenario_1_answer_1 = "\nYou have chosen Option A\n \n    - Increasing Selling Prices by ~5% has resulted in a decrease in projected\n\
     units sold, with customers choosing competitor alternatives\n\
-    \n- Unfortunately overall Revenue projections have declined\n\
-    \n- With expenses unchanged, profit projections have fallen as a result\n"
+        \n- Unfortunately overall Revenue projections have declined\n\
+        \n- With expenses unchanged, profit projections have fallen as a result\n"
     scenario_1_answer_2 = "\nYou have chosen Option B\n \n    - Maintaining Current Selling Prices has resulted in no change to projected\n\
     units sold\n\
     - Overall Revenue projections are unchanged\n\
@@ -245,8 +245,8 @@ def question_2(revenue1, revenue_delta1, expenses1, expense_delta1, profits1, pr
     units sold, with increasing customer interest\n\
     - Yearly Revenue projection increases run ahead of equivalent expenditure increase projections\n\
     - Profit projections have increased as a result\n"
-    scenario_2_answer_2 = "\nYou have chosen Option B\n \n    - Retaining current marketing expenditure levels has resulted in flat projected\n\
-    unit sales\n\
+    scenario_2_answer_2 = "\nYou have chosen Option B\n \n    - Retaining current marketing expenditure levels has resulted in flat\n\
+    projected unit sales\n\
     - Yearly Revenue projections remain unchanged as a result\n\
     - With expenditure forecasts unchanged, Profit projections remained static\n"
     scenario_2_answer_3 = "\nYou have chosen Option C\n \n    - Decreasing the Marketing Budget by ~5% has resulted in a decrease in projected\n\
@@ -374,9 +374,10 @@ def question_3(revenue2, revenue_delta2, expenses2, expense_delta2, profits2, pr
     unit costs and unit sales\n\
     - Yearly Revenue projections remain unchanged as a result\n\
     - With expenditure forecasts unchanged, Profit projections remained static\n"
-    scenario_3_answer_3 = "\nYou have chosen Option C\n \n    - Decreasing the material supply rate by ~5% has resulted in a decrease in projected\n\
-    unit costs\n\
-    - Yearly Revenue projections are unchanged, however cost projections have decreased\n\
+    scenario_3_answer_3 = "\nYou have chosen Option C\n \n    - Decreasing the material supply rate by ~5% has resulted in a decrease in\n\
+        projected unit costs\n\
+    - Yearly Revenue projections are unchanged, however cost projections have\n\
+        decreased\
     - Profit projections for the year have improved as a result\n"
 
     
